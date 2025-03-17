57a6b734aefae2f91718c6e237a20ec68b403337
lindholmadam <adam-lindholm@hotmail.com>
2025-03-17 21:58:55 +0100

    ui

--- components/About.js
+++ components/About.js
@@ -11,7 +11,7 @@ export default function About() {
         <FadeInWhenVisible>
         <div className="flex flex-col lg:flex-row lg:gap-16">
             <p className="max-w-3xl text-md/6 text-center mx-auto text-[17px] text-gray-300">
-            Hi, I'm a tech enthusiast with a passion for web development and a background in computer and systems sciences and marketing communication. I’m driven by how technology shapes digital business, user experience, and growth. My interests lie at the intersection of development, marketing, and sales, where I enjoy bridging technical and strategic needs. <br/> With my combined background in technology and communication, I thrive in roles that connect development, business, and user experience - solving technical challenges with a strategic mindset to create impactful digital solutions.
+            Hi, I'm a tech enthusiast with a passion for web development and a background in computer and systems sciences and marketing communication. I’m driven by how technology shapes digital business, user experience, and growth. My interests lie at the intersection of development, marketing, and sales, where I enjoy bridging technical and strategic needs. <br/> With my combined background in technology and communication I thrive in roles that connect development, business and user experience - solving technical challenges with a strategic mindset to create impactful digital solutions.
             </p>
         </div>
         </FadeInWhenVisible>
